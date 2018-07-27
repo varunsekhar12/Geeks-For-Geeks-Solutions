b1d79bb97af7d15a656b55cd3334b17afdc37d72
Jagan Kaartik <kaartikjagan@gmail.com>
2018-07-28 00:32:37 +0530

    Add files via upload
    Given an array, print reverse of it.
    
    Input:
    
    First line contains an integer, the number of test cases 'T' Each test case should contain an integer, size of array 'N' in the first line. In the second line Input the integer elements of the array in a single line separated by space.
    
    Output:
    
    Print the array in reverse order in a single line separated by space.Each array is to be printed in separate line.
    
    Constraints:
    
    1 <= T <= 100
    
    1 <= N <=100
    
    0 <= Arr[i] <= 100
    
    Example:
    
    Input
    1
    4
    1 2 3 4
    
    Output:
    4 3 2 1

---
+++ reverse.cpp
@@ -0,0 +1,20 @@
+#include<iostream.h>
+using namespace std;
+int main() 
+{
+    int a[100],b,n,i,size,j;
+    std::cin>>n;
+    for(i=0;i<n;++i)
+    {
+        std::cin>>size;
+        for(j=1;j<=size;++j)
+        {
+            std::cin>>a[j];
+        }
+         for(j=size;j>0;--j)
+        {
+            std::cout<<a[j]<<" ";
+        }
+        std::cout<<endl;
+    }
+}
